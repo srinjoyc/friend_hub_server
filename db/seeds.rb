d316ab6362c3ff6129735dda78e3d838f075bc37
Srinjoy Chakraborty <srinjoy@live.ca>
2017-03-12 20:47:31 -0700

    added postgres as the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!({name:"Srinjoy", email:"srinjoycal@gmail.com", password:"123456", bio:"Hi!"})
