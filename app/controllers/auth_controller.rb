4eb9aa70a6702c677a3cbdb521802785b5c248e5
Srinjoy Chakraborty <srinjoy@live.ca>
2017-03-16 09:33:53 -0700

    handled no user error

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
  
   def login
     @user = User.where(email: params[:email]).first
-    if @user.exists? && @user.password == params[:password]
+    if @user.empty? && @user.password == params[:password]
       render json: @user 
     else
       render :json => "Invalid Credentials", :status => :unauthorized
