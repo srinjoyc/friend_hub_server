51def576b14223d0e7bd6a0575d376c2d6ac5690
Srinjoy Chakraborty <srinjoy@live.ca>
2017-03-16 09:56:00 -0700

    handled no user error

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,16 +1,22 @@
 class AuthController < ApplicationController
  
   def login
-    
+
     @user = User.where(email: params[:email]).first
     if @user.blank?
-      render :json => "No user found" 
+      render :json => {
+        error: "No such user; check the submitted email address",
+        status: 400
+      }, status: 400
       return
     elsif @user.password == params[:password]
       render json: @user 
       return
     else
-      render :json => "Invalid Credentials", :status => :unauthorized
+      render :json => {
+        error: "No such user; check the submitted email address",
+        status: 400
+      }, status: 400
     end 
 
   end
